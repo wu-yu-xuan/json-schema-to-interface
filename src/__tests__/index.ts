2eb4b7b0f08f13c5b85468c8c59ef52dc4dc7ef3
吴聿轩 <929003281@qq.com>
2020-02-23 17:11:24 +0800

    保存翻车现场

---
+++ src/__tests__/index.ts
@@ -0,0 +1,24 @@
+process.env.INPUT_DIR = 'example';
+import { setFailed } from '@actions/core';
+import getFileInfo from '../getFileInfo';
+import inputs from '../inputs';
+import prepareOutput from '../prepareOutput';
+import generateIndex from '../generateIndex';
+import generateFiles from '../generateFiles';
+
+it('test run', async () => {
+  try {
+    const [fileInfos] = await Promise.all([
+      getFileInfo({
+        folder: inputs.inputDir,
+        root: process.cwd()
+      }),
+      prepareOutput()
+    ]);
+    expect(fileInfos).toMatchInlineSnapshot();
+    generateIndex(fileInfos);
+    generateFiles(fileInfos);
+  } catch (e) {
+    setFailed(`Action failed with ${e}`);
+  }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
-import { info, setFailed, getInput } from '@actions/core';
+import { setFailed } from '@actions/core';
+import getFileInfo from './getFileInfo';
+import inputs from './inputs';
+import prepareOutput from './prepareOutput';
+import generateIndex from './generateIndex';
+import generateFiles from './generateFiles';
 
 try {
-  info(`hello ${getInput('name')}!`);
+  const [fileInfos] = await Promise.all([
+    getFileInfo({
+      folder: inputs.inputDir,
+      root: process.cwd()
+    }),
+    prepareOutput()
+  ]);
+  generateIndex(fileInfos);
+  generateFiles(fileInfos);
 } catch (e) {
   setFailed(`Action failed with ${e}`);
 }
