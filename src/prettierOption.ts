7f928dfa5ee42bdf5ffab4b577ed2bf67a3ecbea
吴聿轩 <929003281@qq.com>
2020-02-23 17:40:11 +0800

    transpileAstToCode

---
+++ src/prettierOption.ts
@@ -0,0 +1,13 @@
+import { Options } from 'prettier';
+
+const prettierOptions: Options = {
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false,
+  bracketSpacing: true
+};
+
+export default prettierOptions;
---
+++ src/transpileAstToCode.ts
@@ -0,0 +1,20 @@
+import ts from 'typescript';
+import { format } from 'prettier';
+import prettierOptions from './prettierOption';
+
+export default function transpileAstToCode(ast: ts.Node[]) {
+  const file = ts.createSourceFile(
+    '',
+    '',
+    ts.ScriptTarget.Latest,
+    false,
+    ts.ScriptKind.TS
+  );
+  const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+  const code = printer.printList(
+    ts.ListFormat.AllowTrailingComma,
+    (ast as unknown) as ts.NodeArray<ts.Node>,
+    file
+  );
+  return format(code, prettierOptions);
+}
