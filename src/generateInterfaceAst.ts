b51121e98fdc561bb3e5972e5153f1139eabee7c
吴聿轩 <929003281@qq.com>
2020-02-25 10:24:54 +0800

    update interfaceGenerator

--- src/generateInterfaceAst.ts
+++ src/generateInterfaceAst.ts
@@ -3,40 +3,48 @@ import ts from 'typescript';
 import inputs from './inputs';
 import { basename } from 'path';
 
+interface Interface {
+  [path: string]: {
+    [method: string]: {
+      [requestOrResponse: string]: string;
+    };
+  };
+}
+
+interface CreateTypeLiteralNode {
+  [k: string]: string | CreateTypeLiteralNode;
+}
+
+/**
+ * export default interface ${inputs.interfaceName} {
+ *   ${path}: {
+ *     ${method}: {
+ *       ${requestOrResponse}: ${interfaceName}
+ *     }
+ *   }
+ * }
+ */
 export default function generateInterfaceAst(fileInfos: FileInfo[]) {
-  const temp = fileInfos.reduce<{ [k: string]: { [k: string]: string } }>(
-    (prev, cur) => {
-      const index = cur.relativePath.lastIndexOf('/');
-      prev[cur.relativePath.slice(0, index)] =
-        prev[cur.relativePath.slice(0, index)] ?? {};
-      prev[cur.relativePath.slice(0, index)][
-        basename(cur.relativePath.slice(index + 1), '.json')
-      ] = cur.interfaceName;
-      return prev;
-    },
-    {}
-  );
-  const propertySignatures = Object.entries(temp).map(([path, value]) => {
-    const typeElements = Object.entries(value).map(([name, interfaceName]) =>
-      ts.createPropertySignature(
-        undefined,
-        ts.createIdentifier(name),
-        undefined,
-        ts.createTypeReferenceNode(
-          ts.createIdentifier(interfaceName),
-          undefined
-        ),
-        undefined
-      )
-    );
-    return ts.createPropertySignature(
+  const temp = fileInfos.reduce<Interface>((prev, cur) => {
+    const pathArray = cur.relativePath.split('/');
+    const { length } = pathArray;
+    const path = pathArray.slice(0, length - 2).join('/');
+    const method = pathArray[length - 2];
+    const requestOrResponse = basename(pathArray[length - 1], '.json');
+    prev[path] = prev[path] ?? {};
+    prev[path][method] = prev[path][method] ?? {};
+    prev[path][method][requestOrResponse] = cur.interfaceName;
+    return prev;
+  }, {});
+  const propertySignatures = Object.entries(temp).map(([path, value]) =>
+    ts.createPropertySignature(
       undefined,
       ts.createStringLiteral(path),
       undefined,
-      ts.createTypeLiteralNode(typeElements),
+      createTypeLiteralNode(value),
       undefined
-    );
-  });
+    )
+  );
   return ts.createInterfaceDeclaration(
     undefined,
     [
@@ -47,5 +55,21 @@ export default function generateInterfaceAst(fileInfos: FileInfo[]) {
     undefined,
     undefined,
     propertySignatures
+  );
+}
+
+function createTypeLiteralNode(o: CreateTypeLiteralNode): ts.TypeLiteralNode {
+  return ts.createTypeLiteralNode(
+    Object.entries(o).map(([key, value]) =>
+      ts.createPropertySignature(
+        undefined,
+        ts.createIdentifier(key),
+        undefined,
+        typeof value === 'string'
+          ? ts.createTypeReferenceNode(ts.createIdentifier(value), undefined)
+          : createTypeLiteralNode(value),
+        undefined
+      )
+    )
   );
 }
